a8dbb1a926901887aec55aa7202e0629d0ffc3fc
rodrigo15975 <javierluis15975@hotmail.com>
2024-11-07 23:05:13 -0500

    first commit actions -3

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import './App.css'
+import "./App.css"
 
 function App() {
-
   return (
     <>
-    
+      <h1>
+        Welcome to github actions, i'm learning gituub actions, is very
+        important for developer software
+      </h1>
     </>
   )
 }
